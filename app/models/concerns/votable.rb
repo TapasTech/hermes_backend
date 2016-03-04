c08dd9bcdac82714faab2f7f0d8ea9005954cc76
Karloku Sang <karloku@gmail.com>
2016-03-04 12:13:05 +0800

    voting system using redis

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -6,40 +6,71 @@ module Votable
     has_many :votes, as: :votable
     has_many :users, through: :votes
 
+    include Redis::Objects
+    set :up_voters
+    set :down_voters
+
     # Countable
     def up_votes_count
-      votes.ups.sum(:weight)
+      # votes.ups.sum(:weight)
+      up_voters.count
     end
 
     def down_votes_count
-      votes.downs.sum(:weight)
+      # votes.downs.sum(:weight)
+      down_voters.count
     end
 
     def seperate_votes_counts
-      weights = votes.group(:weight).sum(:weight)
+      # weights = votes.group(:weight).sum(:weight)
+      # {
+      #   -1 => weights.select { |w, _total_w| w < 0 }
+      #                .lazy.map { |_w, total_w| total_w }
+      #                .reduce(&:+) || 0,
+      #   1 => weights.select { |w, _total_w| w > 0 }
+      #               .lazy.map { |_w, total_w| total_w }
+      #               .reduce(&:+) || 0
+      # }
       {
-        -1 => weights.select { |w, _total_w| w < 0 }
-                     .lazy.map { |_w, total_w| total_w }
-                     .reduce(&:+) || 0,
-        1 => weights.select { |w, _total_w| w > 0 }
-                    .lazy.map { |_w, total_w| total_w }
-                    .reduce(&:+) || 0
+        -1 => down_votes_count,
+        1 => up_votes_count
       }
     end
 
     def total_votes_count
-      votes.sum(:weight)
+      # votes.sum(:weight)
+      up_votes_count - down_votes_count
     end
 
     # Votable
     def vote_by(user, weight)
-      vote = votes.find_or_initialize_by(user: user)
-      vote.update!(weight: weight)
+      # vote = votes.find_or_initialize_by(user: user)
+      # vote.update!(weight: weight)
+      if weight > 0
+        up_vote_by(user)
+      elsif weight < 0
+        down_vote_by(user)
+      else
+        unvote_by(user)
+      end
+    end
+
+    def up_vote_by(user)
+      up_voters << user.id
+      down_voters.delete(user.id)
+      update_rank!
+    end
+
+    def down_vote_by(user)
+      down_voters << user.id
+      up_voters.delete(user.id)
       update_rank!
     end
 
     def unvote_by(user)
-      votes.find_or_initialize_by(user: user).destroy!
+      # votes.find_or_initialize_by(user: user).destroy!
+      up_voters.delete(user.id)
+      down_votes_count.delete(user.id)
       update_rank!
     end
 
