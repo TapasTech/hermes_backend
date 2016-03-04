688e412dd84f2dbbc7bc1bfb097f1b845df8d891
Karloku Sang <karloku@gmail.com>
2016-03-04 15:30:26 +0800

    auth_token

--- spec/graph/mutations/auth_tokens_mutation_spec.rb
+++ spec/graph/mutations/auth_tokens_mutation_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 
 RSpec.describe AuthTokensMutation do
   subject { described_class }
-  let(:auth_token) { create(:auth_token) }
+  let(:user) { create(:user) }
+  let(:auth_token) { resolution.auth_token }
 
   describe '::CreateAuthTokenField' do
     let(:resolution) do
       ::AuthTokensMutation::CreateAuthTokenField.resolve(
-        auth_token,
+        nil,
         GraphQL::Query::Arguments.new(arguments),
         context)
     end
@@ -17,13 +18,17 @@ RSpec.describe AuthTokensMutation do
 
     context 'with proper argument and context' do
       let(:arguments) do
-        {}
+        {
+          email: user.email,
+          password: user.password
+        }
       end
 
-      let(:current_user) { user }
+      let(:current_user) { nil }
 
       it 'resolves correctly' do
-        expect(resolution).to be_truthy
+        expect(resolution.user).to eq(user)
+        expect(AuthToken.from_token(auth_token).user).to eq(user)
       end
     end
   end
@@ -31,7 +36,7 @@ RSpec.describe AuthTokensMutation do
   describe '::RenewAuthTokenField' do
     let(:resolution) do
       ::AuthTokensMutation::RenewAuthTokenField.resolve(
-        auth_token,
+        nil,
         GraphQL::Query::Arguments.new(arguments),
         context)
     end
@@ -39,14 +44,13 @@ RSpec.describe AuthTokensMutation do
     let(:context) { {current_user: current_user} }
 
     context 'with proper argument and context' do
-      let(:arguments) do
-        {}
-      end
+      let(:arguments) { {} }
 
       let(:current_user) { user }
 
       it 'resolves correctly' do
-        expect(resolution).to be_truthy
+        expect(resolution.user).to eq(user)
+        expect(AuthToken.from_token(auth_token).user).to eq(user)
       end
     end
   end
