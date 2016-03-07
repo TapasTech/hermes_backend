a0de3c49ee2251e33c4803539c509d289e3d7239
Karloku Sang <karloku@gmail.com>
2016-03-07 18:22:20 +0800

    test setting

--- config/initializers/redis_objects.rb
+++ config/initializers/redis_objects.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
-Redis::Objects.redis = ConnectionPool.new(Settings.redis_objects.pool.to_h) do
-  Redis.new(Settings.redis_objects.client.to_h)
+Redis::Objects.redis = ConnectionPool.new(Settings.redis_objects&.pool.to_h) do
+  Redis.new(Settings.redis_objects&.client.to_h)
 end
