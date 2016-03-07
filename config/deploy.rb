bee3cb0a9350a23cb7d86c34f96e7e0d78d9d956
Karloku Sang <karloku@gmail.com>
2016-03-07 18:09:05 +0800

    rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :shared_paths, [
 task :environment do
   # If you're using rbenv, use this to load the rbenv environment.
   # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
+  invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
   # invoke :'rvm:use[ruby-1.9.3-p125@default]'
