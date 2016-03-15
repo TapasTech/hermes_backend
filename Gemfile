72cdfd3be38f50b438a0c2f5d11379547742f91f
Karloku Sang <karloku@gmail.com>
2016-03-15 13:53:32 +0800

    i18n

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '>= 5.0.0.beta3', '< 5.1'
 gem 'config'
 # Lograge
 gem 'lograge'
+# I18n
+gem 'rails-i18n'
 
 # Modeling
 ## Use postgresql as the database for Active Record
--- app/graph/errors/custom_graphql_errors.rb
+++ app/graph/errors/custom_graphql_errors.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 module CustomGraphQLErrors
-  class Error < StandardError; end
+  BASE_KEY = 'graphql.errors.messages'
+  # Basic Error
+  class Error < StandardError
+    attr_reader :error_key, :message
+
+    def error_key
+      @error_key ||= self.class.name.underscore.tr('/', '.')
+    end
+
+    def message
+      @message ||= ::I18n.t("#{BASE_KEY}.#{error_key}")
+    end
+  end
 
   class AuthenticationError < Error; end
   class WrongPasswordError < Error; end
