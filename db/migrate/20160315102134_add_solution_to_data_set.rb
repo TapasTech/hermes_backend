5864bd9c7b9461df02db4b17e00110dfc104e58c
zhaolongwen <zlw302323@gmail.com>
2016-03-16 11:05:47 +0800

    add reference competition and dataset

--- db/migrate/20160315102134_add_solution_to_data_set.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class AddSolutionToDataSet < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :data_sets, :solution, index: true, foreign_key: true
-  end
-end
