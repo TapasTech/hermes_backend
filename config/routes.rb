3a3785655b120f0194096e4a94591ba45ab78b9a
Karloku Sang <karloku@gmail.com>
2016-03-18 16:46:45 +0800

    remove space

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Rails.application.routes.draw do
   get  'graphql', to: 'graphql#introspection'
   post 'graphql', to: 'graphql#query'
-  
+
   resources :put_tokens, only: [:show]
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
